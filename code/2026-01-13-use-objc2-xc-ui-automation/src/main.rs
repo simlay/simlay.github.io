bcf4a83e0847b7b62c9b7e999cb88cf908ecd52c
Sebastian Imlay <sebastian.imlay@gmail.com>
2026-01-28 16:17:49 -0500

    Finished Initial draft

--- code/2026-01-13-use-objc2-xc-ui-automation/src/main.rs
+++ code/2026-01-13-use-objc2-xc-ui-automation/src/main.rs
@@ -1,3 +1,99 @@
+use objc2::{
+    define_class, msg_send,
+    rc::{Allocated, Retained},
+    runtime::AnyObject,
+    ClassType, Ivars, MainThreadMarker, MainThreadOnly,
+};
+
+use objc2_foundation::{NSDictionary, NSObject, NSObjectProtocol, NSString};
+use objc2_ui_kit::{
+    UIApplication, UIApplicationDelegate, UIApplicationLaunchOptionsKey, UIScreen, UITextField,
+    UIViewController, UIWindow,
+};
+
+define_class!(
+    // SAFETY:
+    // - `NSObject` does not have any subclassing requirements.
+    // - `AppDelegate` does not implement `Drop`.
+    #[unsafe(super(NSObject))]
+    #[thread_kind = MainThreadOnly]
+    struct AppDelegate {
+        window: std::cell::RefCell<Option<Retained<UIWindow>>>,
+    }
+
+    impl AppDelegate {
+        // Called by `UIApplication::main`.
+        #[unsafe(method_id(init))]
+        fn init(this: Allocated<Self>) -> Retained<Self> {
+            let this = this.set_ivars(Ivars::<Self> {
+                window: std::cell::RefCell::new(None),
+            });
+            unsafe { msg_send![super(this), init] }
+        }
+    }
+
+    unsafe impl NSObjectProtocol for AppDelegate {}
+
+    unsafe impl UIApplicationDelegate for AppDelegate {
+        #[unsafe(method(application:didFinishLaunchingWithOptions:))]
+        unsafe fn did_finish_launching_with_options(
+            &self,
+            application: &UIApplication,
+            launch_options: Option<&NSDictionary<UIApplicationLaunchOptionsKey, AnyObject>>,
+        ) -> bool {
+            let mtm = MainThreadMarker::new().expect("Failed to get mtm");
+            let window = UIWindow::new(mtm);
+            window.setFrame(UIScreen::mainScreen(mtm).bounds());
+            window.makeKeyAndVisible();
+            *self.window().borrow_mut() = Some(window);
+
+            let view_controller = UIViewController::new(mtm);
+            let text_field = UITextField::new(mtm);
+            text_field.setText(Some(&NSString::from_str("THIS IS THE DEFAULT TEXT")));
+
+            self.window().borrow().as_ref().unwrap().setRootViewController(Some(&view_controller));
+            view_controller.setView(Some(&text_field));
+            text_field.setBackgroundColor(Some(&objc2_ui_kit::UIColor::whiteColor()));
+            true
+        }
+    }
+);
+
 fn main() {
-    println!("Hello, world!");
+    set_llvm_profile_write();
+    println!("Hello, World!");
+    let mtm = MainThreadMarker::new().unwrap();
+    let delegate_class = NSString::from_class(AppDelegate::class());
+    UIApplication::main(None, Some(&delegate_class), mtm);
+}
+
+
+fn set_llvm_profile_write() {
+    if std::env::var("LLVM_PROFILE_FILE").is_ok() {
+        let _will_terminate_observer = create_observer(
+            &objc2_foundation::NSNotificationCenter::defaultCenter(),
+            unsafe { objc2_ui_kit::UIApplicationDidEnterBackgroundNotification },
+            move |_notification| {
+                unsafe extern "C" {
+                    safe fn __llvm_profile_write_file() -> std::ffi::c_int;
+                }
+                let res = __llvm_profile_write_file();
+                assert_eq!(res, 0);
+            },
+        );
+    }
+}
+
+fn create_observer(
+    center: &objc2_foundation::NSNotificationCenter,
+    name: &objc2_foundation::NSNotificationName,
+    handler: impl Fn(&objc2_foundation::NSNotification) + 'static,
+) -> objc2::rc::Retained<objc2::runtime::ProtocolObject<dyn objc2_foundation::NSObjectProtocol>> {
+    let block = block2::RcBlock::new(
+        move |notification: std::ptr::NonNull<objc2_foundation::NSNotification>| {
+            handler(unsafe { notification.as_ref() });
+        },
+    );
+
+    unsafe { center.addObserverForName_object_queue_usingBlock(Some(name), None, None, &block) }
 }
--- code/2026-01-13-use-objc2-xc-ui-automation/ui_tests/src/main.rs
+++ code/2026-01-13-use-objc2-xc-ui-automation/ui_tests/src/main.rs
@@ -1,2 +1,87 @@
-fn main() {
+#![no_main] // Required, we build this with `-bundle`.
+use objc2::{ClassType, MainThreadOnly, define_class};
+use objc2_foundation::NSString;
+use objc2_foundation::{
+    NSSearchPathDirectory, NSSearchPathDomainMask, NSSearchPathForDirectoriesInDomains,
+    NSDictionary, ns_string,
+};
+use objc2_xc_test::XCTestCase;
+use objc2_xc_ui_automation::{
+    XCUIApplication, XCUIDevice, XCUIElementTypeQueryProvider, XCUIScreenshot,
+    XCUIScreenshotProviding,
+};
+
+define_class!(
+    #[unsafe(super = XCTestCase)]
+    #[thread_kind = MainThreadOnly]
+    struct TestCase;
+
+    impl TestCase {
+        #[unsafe(method(setUp))]
+        fn set_up(&self) {
+            // Test setup code in here.
+        }
+
+        #[unsafe(method(tearDown))]
+        fn tear_down(&self) {
+            // Test teardown code in here.
+        }
+
+        #[unsafe(method(testSimple))]
+        fn test_simple(&self) {
+            let app = XCUIApplication::new(self.mtm());
+
+            // SIMCTL_CHILD_DINGHY_LLVM_PROFILE_FILE
+            if let Ok(val) = std::env::var("DINGHY_LLVM_PROFILE_FILE") {
+                let envs: objc2::rc::Retained<NSDictionary<NSString, NSString>> =
+                    NSDictionary::from_slices(
+                        &[
+                        ns_string!("LLVM_PROFILE_FILE"),
+                        ns_string!("LLVM_PROFILE_VERBOSE_ERRORS"),
+                        ],
+                        &[&NSString::from_str(val.as_str()), ns_string!("1")],
+                    );
+
+                app.setLaunchEnvironment(&envs);
+            }
+
+            app.launch();
+            let text_view = app.textFields().elementBoundByIndex(0);
+            text_view.tap();
+            text_view.typeText(&NSString::from_str(" THIS TEXT IS FROM XCTEST"));
+            save_screenshot(&app.screenshot());
+
+            let device = XCUIDevice::sharedDevice(self.mtm());
+            let siri = device.siriService();
+            siri.activateWithVoiceRecognitionText(&NSString::from_str("What is the capital of germany?"));
+
+            std::thread::sleep(std::time::Duration::from_millis(500));
+
+            device.pressButton(objc2_xc_ui_automation::XCUIDeviceButton::Home);
+            device.pressButton(objc2_xc_ui_automation::XCUIDeviceButton::Home);
+
+        }
+    }
+);
+
+/// Load and initialize the class such that XCTest can see it.
+#[ctor::ctor]
+unsafe fn setup() {
+    let _ = TestCase::class();
+}
+
+fn save_screenshot(screenshot: &XCUIScreenshot) {
+    let path = NSSearchPathForDirectoriesInDomains(
+        NSSearchPathDirectory::DocumentDirectory,
+        NSSearchPathDomainMask::UserDomainMask,
+        true,
+    );
+    if let Some(path) = path.firstObject() {
+        let path = path.to_string();
+        let path = std::path::Path::new(&path).join(format!("screenshot.png"));
+        let res = screenshot
+            .PNGRepresentation()
+            .writeToFile_atomically(&NSString::from_str(path.to_str().unwrap()), false);
+        assert!(res, "failed writing screenshot");
+    }
 }
